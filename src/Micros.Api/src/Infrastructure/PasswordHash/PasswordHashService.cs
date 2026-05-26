a58389c8cfb90346b4586c0a2b51ad713aeacd23
dima5513 <super.dimka5513@yandex.ru>
2026-05-26 18:22:55 +0300

    Сервис хеширования паролей на BCrypt

---
+++ src/Micros.Api/src/Infrastructure/PasswordHash/PasswordHashService.cs
@@ -0,0 +1,10 @@
+﻿namespace Micros.Api.Infrastructure.PasswordHash;
+
+public class PasswordHashService
+{
+    public string Hash(string password) =>
+        BCrypt.Net.BCrypt.HashPassword(password);
+
+    public bool Verify(string password, string passwordHash) =>
+        BCrypt.Net.BCrypt.Verify(password, passwordHash);
+}
