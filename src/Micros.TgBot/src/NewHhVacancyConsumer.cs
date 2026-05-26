6308700dfe94ebd3dfbeba311c2302909f17fa3d
dima5513 <super.dimka5513@yandex.ru>
2026-05-26 18:26:15 +0300

    Консьюмер новых вакансий в TgBot с отправкой уведомления в Telegram

---
+++ src/Micros.TgBot/src/NewHhVacancyConsumer.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using Micros.Core.messages;
+using Micros.Core.rabbitmq;
+using Telegram.Bot;
+
+namespace Micros.TgBot;
+
+public class NewHhVacancyConsumer(
+    ITelegramBotClient tgBotClient,
+    ILogger<NewHhVacancyConsumer> logger
+)
+    : IRabbitMqConsumer
+{
+    public string Exchange => "hh.vacancies";
+    public string QueueName => "tgbot.new-vacancies";
+    public string RoutingKey => "hh.new-vacancy";
+
+    public async Task HandleAsync(string body, CancellationToken ct)
+    {
+        var message = JsonSerializer.Deserialize<NewVacancyMessage>(body)
+                      ?? throw new InvalidOperationException("Failed to deserialize NewVacancyMessage");
+
+        await tgBotClient.SendMessage(
+            chatId: message.TelegramId,
+            text: $"Задача: {message.TaskId}\n\n{message.Name}\n{message.Url}",
+            cancellationToken: ct
+        );
+    }
+}
