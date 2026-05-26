c0b8b6000157cb83bc87d0fa5d10d605f7290ab2
dima5513 <super.dimka5513@yandex.ru>
2026-05-26 18:25:35 +0300

    Миграция колонки telegram_id с уникальным индексом для users

---
+++ src/Micros.Api/Migrations/20260517124736_UserTelegramIdColumn.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Micros.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserTelegramIdColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "telegram_id",
+                table: "users",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_telegram_id",
+                table: "users",
+                column: "telegram_id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_users_telegram_id",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "telegram_id",
+                table: "users");
+        }
+    }
+}
