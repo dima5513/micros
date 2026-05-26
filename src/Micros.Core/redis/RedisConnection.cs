e74fef82d0ffb92cc52b03be62f081589cb0a915
dima5513 <super.dimka5513@yandex.ru>
2026-05-26 18:25:08 +0300

    Инфраструктура Redis: опции и подключение

---
+++ src/Micros.Core/redis/RedisConnection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Options;
+using StackExchange.Redis;
+
+namespace Micros.Core.redis;
+
+public class RedisConnection : IAsyncDisposable
+{
+    private readonly Lazy<Task<IConnectionMultiplexer>> _multiplexer;
+
+    public RedisConnection(IOptions<RedisOptions> options)
+    {
+        var connectionString = $"{options.Value.Host}:{options.Value.Port}";
+        _multiplexer =
+            new Lazy<Task<IConnectionMultiplexer>>(async () =>
+                await ConnectionMultiplexer.ConnectAsync(connectionString));
+    }
+
+    public async Task<IDatabase> GetDatabaseAsync()
+    {
+        var mux = await _multiplexer.Value;
+        return mux.GetDatabase();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_multiplexer.IsValueCreated)
+            await (await _multiplexer.Value).DisposeAsync();
+    }
+}
---
+++ src/Micros.Core/redis/RedisOptions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Micros.Core.redis;
+
+public class RedisOptions
+{
+    [ConfigurationKeyName("REDIS_HOST")]
+    public string Host { get; set; }
+    [ConfigurationKeyName("REDIS_PORT")]
+    public int Port { get; set; }
+}
