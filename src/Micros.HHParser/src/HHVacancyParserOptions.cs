b67f02a59ee606c206093c3bf050fe6794a21a85
dima5513 <super.dimka5513@yandex.ru>
2026-05-26 18:25:52 +0300

    Скелет HHParser: опции, типы подписок и Dockerfile

---
+++ src/Micros.HHParser/src/HHVacancyParserOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Micros.HHParser;
+
+public class HhVacancyParserOptions
+{
+    public string HhHost { get; set; } = "";
+    public string UserAgent { get; set; } = "";
+    public int IntervalSeconds { get; set; } = 60;
+    public int MaxConcurrency { get; set; } = 2;
+}
